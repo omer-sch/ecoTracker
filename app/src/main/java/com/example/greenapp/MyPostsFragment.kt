3d8aecb9d93a16a7112dc36948b1b8705c0274a2
omer-sch <schreiber.omer@gmail.com>
2024-01-21 23:42:37 +0200

    auth commit

---
+++ app/src/main/java/com/example/greenapp/MyPostsFragment.kt
@@ -0,0 +1,115 @@
+package com.example.greenapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ProgressBar
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.greenapp.Model.Model
+import com.example.greenapp.Model.Post
+import com.example.greenapp.Modules.Posts.Adapter.PostsRecyclerAdapter
+import com.example.greenapp.Modules.Posts.PostsRecyclerViewActivity
+import com.example.greenapp.databinding.FragmentPostsBinding
+
+
+class MyPostsFragment : Fragment() {
+
+        var postsRcyclerView: RecyclerView? = null
+        var posts: List<Post>? = null
+        var adapter: PostsRecyclerAdapter? = null
+        var progressBar: ProgressBar? = null
+
+        private var _binding: FragmentPostsBinding? = null
+        private val binding get() = _binding!!
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+
+            _binding = FragmentPostsBinding.inflate(inflater, container, false)
+            val view = binding.root
+
+            progressBar = binding.progressBar
+
+            progressBar?.visibility = View.VISIBLE
+
+
+            Model.instance.getMyPosts("omer") { posts ->
+                this.posts = posts
+                adapter?.posts = posts
+                adapter?.notifyDataSetChanged()
+
+                progressBar?.visibility = View.GONE
+            }
+
+            postsRcyclerView = binding.rvPostsFragmentList
+            postsRcyclerView?.setHasFixedSize(true)
+            postsRcyclerView?.layoutManager = LinearLayoutManager(context)
+            adapter = PostsRecyclerAdapter(posts)
+            adapter?.listener = object : PostsRecyclerViewActivity.OnItemClickListener {
+
+                override fun onItemClick(position: Int) {
+                    Log.i("TAG", "PostsRecyclerAdapter: Position clicked $position")
+                    val post = posts?.get(position)
+//                post?.let {
+//                    val action = PostsFragmentDirections.actionStudentsFragmentToBlueFragment(it.name)
+//                    Navigation.findNavController(view).navigate(action)
+//                }
+                }
+
+                override fun onStudentClicked(post: Post?) {
+                    Log.i("TAG", "POST $post")
+                }
+            }
+
+            postsRcyclerView?.adapter = adapter
+
+          //  val addPostButton: ImageButton = view.findViewById(R.id.ibtnPostsFragmentAddPost)
+
+           // val action = Navigation.createNavigateOnClickListener(R.id.action_feedFragment_to_addPostFragment)
+           // addPostButton.setOnClickListener(action)
+
+            return view
+        }
+
+        override fun onResume() {
+            super.onResume()
+
+            progressBar?.visibility = View.VISIBLE
+
+            Model.instance.getMyPosts("omer") { posts ->
+                this.posts = posts
+                adapter?.posts = posts
+                adapter?.notifyDataSetChanged()
+
+                progressBar?.visibility = View.GONE
+            }
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+
+            _binding = null
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/greenapp/MyTipsFragment.kt
@@ -0,0 +1,33 @@
+package com.example.greenapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+
+
+class MyTipsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_tips, container, false)
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+}
